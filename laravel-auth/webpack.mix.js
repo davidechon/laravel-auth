ad26cebe24d4f1b08e4f36a64479d41aaee92d4c
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-06-23 16:07:34 +0200

    finito scaffolding

--- laravel-auth/webpack.mix.js
+++ laravel-auth/webpack.mix.js
@@ -11,6 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
- mix.js('resources/js/app.js', 'public/js')
- .sass('resources/sass/app.scss', 'public/css').options({
-     processCssUrls: false}).copyDirectory('./node_modules/@fortawesome/fontawesome-free/webfonts/*', 'public/fonts/-font-awesome');
+//  mix.js('resources/js/app.js', 'public/js')
+//  .sass('resources/sass/app.scss', 'public/css').options({
+//      processCssUrls: false}).copyDirectory('./node_modules/@fortawesome/fontawesome-free/webfonts/*', 'public/fonts/-font-awesome');
+
+// mix.js('resources/js/app.js', 'public/js').vue()
+//     .postCss('resources/css/app.css', 'public/css', [
+//         //
+//     ]);
+mix.js('resources/js/app.js', 'public/js').vue()
+.sass('resources/sass/app.scss', 'public/css').options({
+    processCssUrls: false}).copyDirectory('./node_modules/@fortawesome/fontawesome-free/webfonts/*', 'public/fonts/-font-awesome');
