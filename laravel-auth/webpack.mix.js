d516bb329b819d7450d86e03d85248c845305a34
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-06-24 15:17:02 +0200

    step 1 completo

--- laravel-auth/webpack.mix.js
+++ laravel-auth/webpack.mix.js
@@ -19,6 +19,13 @@ const mix = require('laravel-mix');
 //     .postCss('resources/css/app.css', 'public/css', [
 //         //
 //     ]);
-mix.js('resources/js/app.js', 'public/js').vue()
+
+
+mix.js('resources/js/app.js', 'public/js')
 .sass('resources/sass/app.scss', 'public/css').options({
-    processCssUrls: false}).copyDirectory('./node_modules/@fortawesome/fontawesome-free/webfonts/*', 'public/fonts/-font-awesome');
+    processCssUrls: false}).copyDirectory('./node_modules/@fortawesome/fontawesome-free/webfonts/*', 'public/fonts/font-awesome');
+
+// front-office
+mix.js('resources/js/front.js', 'public/js')
+.sass('resources/sass/front.scss', 'public/css').options({
+    processCssUrls: false});
