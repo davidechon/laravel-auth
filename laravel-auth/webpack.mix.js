c3389713c6fb5462fc3a955276ac25e80f364a56
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-06-26 16:39:57 +0200

    aggiunte parti front

--- laravel-auth/webpack.mix.js
+++ laravel-auth/webpack.mix.js
@@ -11,6 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
+ // back-office
  mix.js('resources/js/app.js', 'public/js')
  .sass('resources/sass/app.scss', 'public/css').options({
      processCssUrls: false}).copyDirectory('./node_modules/@fortawesome/fontawesome-free/webfonts/*', 'public/fonts/-font-awesome');
+
+// front-office
+mix.js('resources/js/front.js', 'public/js')
+.sass('resources/sass/front.scss', 'public/css').options({
+    processCssUrls: false})
